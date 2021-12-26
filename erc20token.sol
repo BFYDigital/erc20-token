f52dc0070aa5c9c1442fc3818834262d5b45aaca
BFYDigital <96664711+BFYDigital@users.noreply.github.com>
2021-12-26 10:52:28 +0300

    Update erc20token.sol

--- erc20token.sol
+++ erc20token.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.2;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0-solc-0.7/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BFYToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BFY Token", "BFY") public {
-        _mint(msg.sender, initialSupply);
+contract BFYToken is ERC20, ERC20Burnable, Ownable {    
+    uint256 private _maxSupply = 10000000 * 10 ** decimals();
+
+    constructor() ERC20("BFY Digital", "BFY") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
+
+     function getMaxSupply() public view returns (uint256) {
+         return _maxSupply;
+     }
+
+     function mint(address to, uint256 amount) public onlyOwner {
+         require(totalSupply() + amount <= getMaxSupply(), "Total is more the max supply.");
+         _mint(to, amount);
+     }
 }
